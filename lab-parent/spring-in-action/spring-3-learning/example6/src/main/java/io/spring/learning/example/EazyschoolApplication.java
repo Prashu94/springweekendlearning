81d5d607b4518ae48a560e77da856b47d8e41525
Prashant <prashantbhat94@gmail.com>
2024-10-13 22:04:09 +0530

    "Adding the correct files for example 40"

---
+++ lab-parent/spring-in-action/spring-3-learning/example6/src/main/java/io/spring/learning/example/EazyschoolApplication.java
@@ -0,0 +1,19 @@
+package io.spring.learning.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories("io.spring.learning.example.repository")
+@EntityScan("io.spring.learning.example.model")
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+public class EazyschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazyschoolApplication.class, args);
+	}
+
+}
