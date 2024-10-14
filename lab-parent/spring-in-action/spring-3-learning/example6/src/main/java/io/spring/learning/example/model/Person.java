af5cac4d5bb7dc1e3f1e52a7ef4915d7cd4b9b6e
Prashant <prashantbhat94@gmail.com>
2024-10-14 22:28:49 +0530

    "Learning Regarding One TO Many Relation and some more security features" - Rememeber to mvn clean install if you copy pasted the html files

--- lab-parent/spring-in-action/spring-3-learning/example6/src/main/java/io/spring/learning/example/model/Person.java
+++ lab-parent/spring-in-action/spring-3-learning/example6/src/main/java/io/spring/learning/example/model/Person.java
@@ -3,6 +3,8 @@ package io.spring.learning.example.model;
 import io.spring.learning.example.annotation.FieldsValueMatch;
 import io.spring.learning.example.annotation.PasswordValidator;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
 import jakarta.persistence.*;
@@ -11,7 +13,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
-@Data
+@Getter
+@Setter
 @Entity
 @FieldsValueMatch.List({
         @FieldsValueMatch(
@@ -66,4 +69,8 @@ public class Person extends BaseEntity{
     @OneToOne(fetch = FetchType.EAGER,cascade = CascadeType.ALL, targetEntity = Address.class)
     @JoinColumn(name = "address_id", referencedColumnName = "addressId",nullable = true)
     private Address address;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = true)
+    @JoinColumn(name = "class_id", referencedColumnName = "classId", nullable = true)
+    private EazyClass eazyClass;
 }
